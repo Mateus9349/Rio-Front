fc81323808c91458a33c46de7210ae905f49c03a
unknown <mateusvpdl@outlook.com>
2025-06-11 13:10:19 -0300

    corrigindo vercel

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://mapadeplantio.com.br',
+  baseURL: 'https://mapadeplantio.com.br',
   headers: {
     'Content-Type': 'application/json',
     'x-api-key': '3a2de9bc56463e72d5190206faf4bff3c4985311ff57c620546a746bf376542e',
